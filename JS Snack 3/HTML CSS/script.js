832e695b68a01394feca9fac9ceb16759b3de107
VincenzoVitello <92366183+VincenzoVitello@users.noreply.github.com>
2021-12-16 16:34:24 +0100

    JS Snack 3 + css finito

---
+++ JS Snack 3/HTML CSS/script.js
@@ -0,0 +1,26 @@
+// Crea due tag div con due id diversi:
+// un div avrà il testo colorato di rosso mentre l’altro di verde.
+// Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+const numberBox = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+const evenNumbers = [];
+const oddNumbers = [];
+
+for (let i = 0; i < numberBox.length; i++) {
+    if(i % 2 == 1) { // index is even
+        evenNumbers.push(numberBox[i]);
+    }else{
+        oddNumbers.push(numberBox[i]);
+    }
+}
+console.log(evenNumbers);
+console.log(oddNumbers);
+
+document.getElementById("green").innerHTML = "Numeri pari: " + evenNumbers;
+document.getElementById("red").innerHTML = "Numeri dispari: " + oddNumbers;
+
+
+// document.getElementById(verde).innerHTML(evenNumbers);
+// document.getElementById(rosso).innerHTML(oddNumbers);
+
+
